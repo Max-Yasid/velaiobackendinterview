fbd439468d182f86c25fe2362a8190f88b5fde79
max_yasid <max.fsmy@gmail.com>
2024-09-28 18:16:57 -0500

    fix delete task response

--- src/modules/task/controllers/deleteById.ts
+++ src/modules/task/controllers/deleteById.ts
@@ -5,7 +5,7 @@ export const deleteById: RouteController = async (req, res) => {
   try {
     const repository = new TaskPostgresRepository();
     const tasks = await repository.delete(req.params.id);
-    res.json(tasks.affected > 1);
+    res.json(tasks.affected > 0);
   } catch (err) {
     res.status(500).json("Error al eliminar tarea");
   }
